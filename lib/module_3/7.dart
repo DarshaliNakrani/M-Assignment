72c5cb448d44935a5640e3f336429a697e24812d
Darshali Nakrani <108445495+darshalipatel@users.noreply.github.com>
2023-01-07 11:02:11 +0530

    module added

--- lib/module_3/7.dart
+++ lib/module_3/7.dart
@@ -1,87 +1,61 @@
 // 7. What are different menus in android. Explain each with example
 
-// //import 'package:assignment_flutter/module3/model/GetLinkPage.dart';
-// //import 'package:assignment_flutter/module3/model/SettingsPage.dart';
-// //import 'package:assignment_flutter/module3/model/SharePage.dart';
-// import 'package:flutter/material.dart';
-//
-// void main() => runApp(MyApp());
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Material App',
-//       home: mainpage(),
-//     );
-//   }
-// }
-//
-// class mainpage extends StatelessWidget {
-//   const mainpage({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Settings Menu'),
-//         actions: [
-//           PopupMenuButton(
-//             // color: Colors.grey,
-//             onSelected: (item) => onSelected(context, item),
-//             itemBuilder: (context) => [
-//               PopupMenuItem(value: 0,
-//                 child: Row(
-//                   children: [
-//                     Icon(Icons.settings, color: Colors.black),
-//                     SizedBox(width: 12,),
-//                     Text('Setting'),
-//                   ],
-//                 ),),
-//               PopupMenuItem(
-//                 value: 1,
-//                 child: Row(
-//                   children: [
-//                     Icon(Icons.share, color: Colors.black),
-//                     SizedBox(width: 12,),
-//                     Text('Share'),
-//                   ],
-//                 ),
-//               ),
-//               PopupMenuItem(value: 2,
-//                 child: Row(
-//                   children: [
-//                     Icon(Icons.phonelink, color: Colors.black),
-//                     SizedBox(width: 12,),
-//                     Text('Get Link'),
-//                   ],
-//                 ),
-//                   ),
-//             ],
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   onSelected(BuildContext context, Object? item) {
-//     switch (item) {
-//       case 0:
-//         Navigator.of(context).push(
-//           MaterialPageRoute(builder: (context) => SettingsPage()),
-//         );
-//         break;
-//       case 1:
-//         Navigator.of(context).push(
-//           MaterialPageRoute(builder: (context) => Sharepage()),
-//         );
-//         break;
-//       case 2:
-//         Navigator.of(context).push(
-//           MaterialPageRoute(builder: (context) => GetLinkPage()),
-//         );
-//         break;
-//     }
-//   }
-// }
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home:d7(),
+    );
+  }
+}
+class d7 extends StatefulWidget {
+  const d7({Key? key}) : super(key: key);
+
+  @override
+  State<d7> createState() => _d7State();
+}
+
+class _d7State extends State<d7> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Seach here',
+                  ),
+                );
+              },
+              icon:Icon(
+                Icons.search,
+              )),
+          PopupMenuButton(itemBuilder: (context) => [
+            PopupMenuItem(
+                value: '',
+                child: Text('Null')
+            ),
+            PopupMenuItem(
+                value: '',
+                child: Text('Null')
+            ),
+          ]),
+        ],
+        centerTitle: true,
+        title: Text('D7'),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Menu of Flutter',style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
+}
